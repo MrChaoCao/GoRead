249a82c3489245abbc3dd234de4d4e7d521d9f4c
Chao Cao <mailbox.chao.cao@gmail.com>
2018-02-01 13:38:22 -0800

    Reroute routes with default format

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :users
+namespace :api, defaults: {format: :json} do
+  resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
