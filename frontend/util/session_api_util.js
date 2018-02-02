45b30a34b1ca1ef76021313b26ad832e994d7ba7
Chao Cao <mailbox.chao.cao@gmail.com>
2018-02-01 17:52:05 -0800

    Add Session actions and reducers

--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -1,13 +1,22 @@
-export const createSession = session => (
+export const postUser = user => (
   $.ajax({
-    method: "POST",
-    url: `api/session`,
+    url: '/api/users',
+    method: 'POST',
+    data: { user },
   })
-)
+);
 
-export const deleteSession = session => (
+export const postSession = user => (
   $.ajax({
-    method: "DELETE",
-    url: `api/session`,
+    url: '/api/session',
+    method: 'POST',
+    data: { user },
   })
-)
+);
+
+export const deleteSession = () => (
+  $.ajax({
+    url: '/api/session',
+    method: 'DELETE',
+  })
+);
