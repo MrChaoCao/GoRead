2821fd295bda0fd9b033256331a0232f7931e355
Chao Cao <mailbox.chao.cao@gmail.com>
2018-02-06 10:54:26 -0800

    Add book table, controller, migration

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:new, :create, :show]
     resource :session, only: [:new, :create, :destroy]
+    resources :books, only: [:index, :show]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
