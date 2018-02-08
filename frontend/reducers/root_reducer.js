1f06285081fa5b4ad4798d94c25c8ccddb76047d
Chao Cao <mailbox.chao.cao@gmail.com>
2018-02-08 14:46:26 -0800

    Add shelf reducer to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import sessionReducer from './session_reducer';
 import sessionErrorsReducer from './session_errors_reducer'
 import booksReducer from './books_reducer'
+import shelvesReducer from './shelves_reducer'
 
 export default combineReducers({
   session: sessionReducer,
   session_errors: sessionErrorsReducer,
-  books: booksReducer
+  books: booksReducer,
+  shelves: shelvesREducer,
 })
---
+++ frontend/reducers/shelves_reducer.js
@@ -0,0 +1,24 @@
+import {
+  RECEIVE_ALL_SHELVES,
+  RECEIVE_SHELF,
+  REMOVE_SHELF,
+} from '../actions/shelf_actions';
+
+export default(state = {}, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_ALL_SHELVES:
+      return merge({}, action.shelves });
+    case RECEIVE_SHELF:
+      return merge({}, state, {[action.shelf.id]: action.shelf});
+    case REMOVE_SHELF:
+      let newState = merge({}, state );
+      delete newSTate[action.shelfId];
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default ShelvesReducer;
